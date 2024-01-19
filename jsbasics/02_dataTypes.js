f364d79dde7557cf92f5856191bdf5ee4b760845
Chaitrali21-4 <118073132+Chaitrali21-4@users.noreply.github.com>
2024-01-19 08:16:43 +0000

    Datatypes study

---
+++ jsbasics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all JS code as a newer version
+
+//alert(3+3)  //we are using nodejs not browser thats why this type of declaration is not allowed.
+
+console.log(3+3)
+console.log("Chaitrali")
+
+let name = "Chaitrali"
+let age = 18 
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false 
+// null => standalone value 
+// undefined => 
+// symbol  => unique
+
+// object
+
+console.log(typeof "Chaitrali");  // string
+console.log(typeof age);  // number
+console.log(typeof null);   // object
+console.log(typeof undefined);  //undefined
+console.log(typeof Boolean);  //function
+console.log(typeof true);  // boolean
